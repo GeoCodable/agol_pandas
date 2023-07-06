43e0597d9a6d503797bec72f83d2e888e05e9ede
GeoCodable <106251341+GeoCodable@users.noreply.github.com>
2023-07-06 15:10:19 -0500

    Update agol_pandas.py testing w/o try stmt

--- src/agol_pandas.py
+++ src/agol_pandas.py
@@ -154,108 +154,108 @@ def df_to_agol_hosted_table(gis, df, item_id, mode='append',
               }
             ]                              
     """        
+    # try:
+    results = []
+    tmp_csv = None
+    tmp_table = None
+
+    # check the supplied mode
+    modes = ["append", "overwrite", "upsert", "update", "insert"]
+    if mode not in modes:
+        raise ValueError(f'Unidentified mode supplied: "{mode}"')
+
+    # Check if the dataframe is empty
+    if len(df) == 0:
+        raise ValueError("The dataframe is empty.")
+
+    # attempt to convert datetime stamps to UTC TZ for AGOL
     try:
-        results = []
-        tmp_csv = None
-        tmp_table = None
+        df = convert_dts_utc(df)
+    except:
+        pass
 
-        # check the supplied mode
-        modes = ["append", "overwrite", "upsert", "update", "insert"]
-        if mode not in modes:
-            raise ValueError(f'Unidentified mode supplied: "{mode}"')
+    # get the target item table
+    item = gis.content.search(item_id)[0]
 
-        # Check if the dataframe is empty
-        if len(df) == 0:
-            raise ValueError("The dataframe is empty.")
+    # determine if the item has layers/tables
+    if bool(item.layers):
+        tgt_table = item.layers[0]   
+    if bool(item.tables):
+        tgt_table = item.tables[0]
 
-        # attempt to convert datetime stamps to UTC TZ for AGOL
-        try:
-            df = convert_dts_utc(df)
-        except:
-            pass
+    # set the append params
+    upsert=False
+    skip_inserts=False
+    skip_updates=False
+    upsert_matching_field=None
 
-        # get the target item table
-        item = gis.content.search(item_id)[0]
+    if mode == 'overwrite':
+        tgt_table.manager.truncate()
 
-        # determine if the item has layers/tables
-        if bool(item.layers):
-            tgt_table = item.layers[0]   
-        if bool(item.tables):
-            tgt_table = item.tables[0]
+    elif mode in ['upsert', 'update', 'insert']:
+        if not upsert_column:
+            raise ValueError("""Upsert, update, and insert, require a column with unique keys must be identified.\n
+                             See: https://doc.arcgis.com/en/arcgis-online/manage-data/add-unique-constraint.htm""")
+        if mode =='update':
+            skip_inserts=True
+        if mode =='insert':
+            skip_updates=True            
+        upsert=True
+        upsert_matching_field=upsert_column
 
-        # set the append params
-        upsert=False
-        skip_inserts=False
-        skip_updates=False
-        upsert_matching_field=None
+    # Split the dataframe into chunks
+    if len(df) > chunk_size:
+        chunks = [df[i:i+chunk_size] for i in range(0,df.shape[0],chunk_size)]
+    else:
+        chunks = [df]
+    if not bool(chunks):
+        raise ValueError("The dataframe could not be chunked, see chunk_size")
 
-        if mode == 'overwrite':
-            tgt_table.manager.truncate()
+    # iterate the chunks and apply the data from the dataframe
+    for idx, chunk in enumerate(chunks):
+        # create a temp csv file path
+        tmp_csv = get_temp_file()
+        chunk.to_csv(tmp_csv)
+        # set the item properties dataframe
+        item_properties = {"title" : tmp_csv}
+        # add/upload the csv to the user's content 
+        tmp_table = gis.content.add(data=tmp_csv , 
+                                     item_properties=item_properties)
+        # get info about the file including fields types and sample records
+        src_info = gis.content.analyze(item=tmp_table.id, 
+                                       file_type='csv', 
+                                       location_type='none')
 
-        elif mode in ['upsert', 'update', 'insert']:
-            if not upsert_column:
-                raise ValueError("""Upsert, update, and insert, require a column with unique keys must be identified.\n
-                                 See: https://doc.arcgis.com/en/arcgis-online/manage-data/add-unique-constraint.htm""")
-            if mode =='update':
-                skip_inserts=True
-            if mode =='insert':
-                skip_updates=True            
-            upsert=True
-            upsert_matching_field=upsert_column
-
-        # Split the dataframe into chunks
-        if len(df) > chunk_size:
-            chunks = [df[i:i+chunk_size] for i in range(0,df.shape[0],chunk_size)]
-        else:
-            chunks = [df]
-        if not bool(chunks):
-            raise ValueError("The dataframe could not be chunked, see chunk_size")
-
-        # iterate the chunks and apply the data from the dataframe
-        for idx, chunk in enumerate(chunks):
-            # create a temp csv file path
-            tmp_csv = get_temp_file()
-            chunk.to_csv(tmp_csv)
-            # set the item properties dataframe
-            item_properties = {"title" : tmp_csv}
-            # add/upload the csv to the user's content 
-            tmp_table = gis.content.add(data=tmp_csv , 
-                                         item_properties=item_properties)
-            # get info about the file including fields types and sample records
-            src_info = gis.content.analyze(item=tmp_table.id, 
-                                           file_type='csv', 
-                                           location_type='none')
-
-            result = tgt_table.append(  item_id=tmp_table.id,
-                                        upload_format="csv",
-                                        source_info=src_info['publishParameters'],
-                                        upsert=upsert,
-                                        skip_updates=skip_updates,
-                                        use_globalids=False,
-                                        update_geometry=False,
-                                        append_fields= df.columns.to_list(),
-                                        rollback=True,
-                                        skip_inserts=skip_inserts,
-                                        upsert_matching_field=upsert_matching_field)
-            tmp_table.delete()
-            results.append({'chunk_id': (idx+1), 
-                            'chunk_size': len(chunk),
-                            'mode' : mode, 
-                            'result': result})
-        return results
-    except Exception as e:
-        print(e)
-    finally:
-        try:
-            if tmp_csv and os.path.exists(tmp_csv):
-                os.remove(tmp_csv)
-        except:
-            pass                
-        try: 
-            if bool(tmp_table):
-                tmp_table.delete()
-        except:
-            pass
+        result = tgt_table.append(  item_id=tmp_table.id,
+                                    upload_format="csv",
+                                    source_info=src_info['publishParameters'],
+                                    upsert=upsert,
+                                    skip_updates=skip_updates,
+                                    use_globalids=False,
+                                    update_geometry=False,
+                                    append_fields= df.columns.to_list(),
+                                    rollback=True,
+                                    skip_inserts=skip_inserts,
+                                    upsert_matching_field=upsert_matching_field)
+        tmp_table.delete()
+        results.append({'chunk_id': (idx+1), 
+                        'chunk_size': len(chunk),
+                        'mode' : mode, 
+                        'result': result})
+    return results
+    # except Exception as e:
+    #     print(e)
+    # finally:
+    #     try:
+    #         if tmp_csv and os.path.exists(tmp_csv):
+    #             os.remove(tmp_csv)
+    #     except:
+    #         pass                
+    #     try: 
+    #         if bool(tmp_table):
+    #             tmp_table.delete()
+    #     except:
+    #         pass
 #-------------------------------------------------------------------------------
 def create_table(gis: GIS, name: str, df: pd.DataFrame):
     """Internal function to upload a new
@@ -329,60 +329,60 @@ def create_hosted_table_from_dataframe(gis: GIS, name: str, df: pd.DataFrame,
     arcgis.gis.Item
         arcgis.gis table layer item/object
     """
-    #try:
-    # Check if the dataframe is empty
-    if len(df) == 0:
-        raise ValueError("The dataframe is empty.")
-
-    # format the service name
-    tbl_name = normalize_service_name(name)
-
-    # Check if the name is already in use
-    name_avail = gis.content.is_service_name_available(tbl_name, "featureService")
-    if not name_avail:
-        qs = f'title:{name} AND owner:{gis.users.me.username} AND type:Feature Service'
-        qr = gis.content.search(qs)[0]
-        qr_link = f'{gis.url}/home/item.html?id={qr.itemid}'
-        print(f'Error service name:({tbl_name}) already exists! SEE: {qr_link}')
-        return qr
-
-    # attempt to convert datetime stamps to UTC TZ for AGOL
     try:
-        df = convert_dts_utc(df)
-    except:
-        pass
-
-    # Split the dataframe into chunks
-    if len(df) > chunk_size:
-        chunks = [df[i:i+chunk_size] for i in range(0,df.shape[0],chunk_size)]
-    else:
-        chunks = [df]
-    if not bool(chunks):
-        raise ValueError("The dataframe could not be chunked, see chunk_size")
-
-    # create a new table using the first chunk, append for subsequent chunks 
-
-    items = gis.content.search(query=f"title:{tbl_name} AND type:Feature Service AND owner:{gis.users.me.username}")
-    items = [i for i in items if i.title==tbl_name]
-    if len(items) > 0 :
-        table_id = items[0].id
-        pub_table = items[0]
-    else:
-        table_id = None
-        pub_table = None
-    for idx, chunk in enumerate(chunks):
-        if idx == 0 and not bool(table_id):
-            pub_table = create_table(gis, name=tbl_name, df=chunk)
-            if not pub_table:
-                raise ValueError("Table could not be published")
-            table_id = pub_table.id
+        # Check if the dataframe is empty
+        if len(df) == 0:
+            raise ValueError("The dataframe is empty.")
+    
+        # format the service name
+        tbl_name = normalize_service_name(name)
+    
+        # Check if the name is already in use
+        name_avail = gis.content.is_service_name_available(tbl_name, "featureService")
+        if not name_avail:
+            qs = f'title:{name} AND owner:{gis.users.me.username} AND type:Feature Service'
+            qr = gis.content.search(qs)[0]
+            qr_link = f'{gis.url}/home/item.html?id={qr.itemid}'
+            print(f'Error service name:({tbl_name}) already exists! SEE: {qr_link}')
+            return qr
+    
+        # attempt to convert datetime stamps to UTC TZ for AGOL
+        try:
+            df = convert_dts_utc(df)
+        except:
+            pass
+    
+        # Split the dataframe into chunks
+        if len(df) > chunk_size:
+            chunks = [df[i:i+chunk_size] for i in range(0,df.shape[0],chunk_size)]
         else:
-            df_to_agol_hosted_table(gis, 
-                                    chunk, 
-                                    table_id, 
-                                    mode='append',
-                                    chunk_size=chunk_size)
-    return pub_table             
-    #except Exception as e:
-        #print(e)
-        #return(e)
+            chunks = [df]
+        if not bool(chunks):
+            raise ValueError("The dataframe could not be chunked, see chunk_size")
+    
+        # create a new table using the first chunk, append for subsequent chunks 
+    
+        items = gis.content.search(query=f"title:{tbl_name} AND type:Feature Service AND owner:{gis.users.me.username}")
+        items = [i for i in items if i.title==tbl_name]
+        if len(items) > 0 :
+            table_id = items[0].id
+            pub_table = items[0]
+        else:
+            table_id = None
+            pub_table = None
+        for idx, chunk in enumerate(chunks):
+            if idx == 0 and not bool(table_id):
+                pub_table = create_table(gis, name=tbl_name, df=chunk)
+                if not pub_table:
+                    raise ValueError("Table could not be published")
+                table_id = pub_table.id
+            else:
+                df_to_agol_hosted_table(gis, 
+                                        chunk, 
+                                        table_id, 
+                                        mode='append',
+                                        chunk_size=chunk_size)
+        return pub_table             
+    except Exception as e:
+        print(e)
+        return(e)
