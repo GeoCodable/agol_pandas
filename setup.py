b2afab96966ef9606e2f5eecf303c0f21275f7c9
GeoCodable <106251341+GeoCodable@users.noreply.github.com>
2023-07-19 13:43:00 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setuptools.setup(
     package_dir={'':'src'},                 # directory of the source code of the package
     install_requires=[                      # package dependencies
                         'arcgis==1.9.1',
-                        'pandas',
-                        'tqdm>=4.64.1',
+                        'pandas>=1.1'
                     ]
     )
